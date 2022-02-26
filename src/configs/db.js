cdf9157d46d0ae97395ad9cc87267ffc592f4c49
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2022-02-27 03:30:52 +0530

    Delete src directory

--- src/configs/db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const mongoose = require("mongoose")
-
-module.exports = () => {
-    return mongoose.connect(`mongodb+srv://thisissaurav:${process.env.DB_PASS}@cluster0.ictln.mongodb.net/product?retryWrites=true&w=majority`)
-}
